a424bb0d41cc32280e9dae17e727237d90a14865
Ticia <ticiarodela@gmail.com>
2020-07-13 22:05:55 -0700

    fixed code

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use(routes)
 
 
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/restaurants")
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/restaurants").then(()=>{
+console.log(process.env.MONGODB_URI)});
 app.listen(PORT, function(){
     console.log("app is listening on port" + PORT)
 })
