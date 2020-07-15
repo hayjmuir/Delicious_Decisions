1ecfa513c0f5facfef252b8b77c8b3111a1bc8d2
Ticia <ticiarodela@gmail.com>
2020-07-14 22:12:08 -0700

    save

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -10,29 +10,29 @@ var path = require("path")
 
     })
 
-    // router.post("/api/restaurant", function (req, res) {
-    //     db.Restaurant.create(req.body).then(function (results) {
-    //         res.json(results)
-    //     })
-    // })
+    router.post("/api/restaurant", function (req, res) {
+        db.Restaurant.create(req.body).then(function (results) {
+            res.json(results)
+        })
+    })
 
-    // router.put("/api/restaurant/:id", function (req, res) {
-    //     db.Restaurant.update({ _id: req.params.id }).then(function (results) {
-    //         res.json(results)
-    //     })
-    // })
-    // router.delete("/api/restaurant/:id", function (req, res) {
-    //     db.Restaurant.remove({ _id: req.params.id }).then(function (results) {
-    //         res.json(results)
-    //     })
+    router.put("/api/restaurant/:id", function (req, res) {
+        db.Restaurant.update({ _id: req.params.id }).then(function (results) {
+            res.json(results)
+        })
+    })
+    router.delete("/api/restaurant/:id", function (req, res) {
+        db.Restaurant.remove({ _id: req.params.id }).then(function (results) {
+            res.json(results)
+        })
 
-    // })
+    })
 
-    // router.post("/api/order", function (req, res) {
-    //     var order=req.body
-    //     console.log("this is the order", order)
-    //     res.end()
-    // })
+    router.post("/api/order", function (req, res) {
+        var order=req.body
+        console.log("this is the order", order)
+        res.end()
+    })
 
 
     
